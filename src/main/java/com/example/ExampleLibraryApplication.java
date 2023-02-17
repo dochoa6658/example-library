c17c65a7ccb3c675e0769373ee8dcec9f0251942
dochoa6658 <36579966+dochoa6658@users.noreply.github.com>
2023-02-16 19:59:32 -0500

    Initial commit

---
+++ src/main/java/com/example/ExampleLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleLibraryApplication.class, args);
+	}
+
+}
